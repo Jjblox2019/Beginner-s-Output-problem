4b096c0fed2ae0284656d89ae17c2a4b44669828
Jjblox2019 <126438138+Jjblox2019@users.noreply.github.com>
2024-05-15 14:58:45 +1200

    HW1.py

--- HW1.py
+++ HW1.py
@@ -1,3 +1,17 @@
+Question 1
 minutes = int(input("give me a number"))
 seconds = minutes * 60
 print(minutes,"minutes is equalto", seconds, "seconds")
+
+Question 2
+cruella_cards = int(input("Enter the number of cards Cruella received for her last birthday: "))
+print("Jane receives at least", cruella_cards * 2, "cards.")
+
+question 3
+age = int(input("Enter your age in years: "))
+
+age_in_days = age * 365
+print("You are", age_in_days, "days old.")
+
+remaining_days = (100 - age) * 365
+print("In", remaining_days, "days you will be 100 years old!")
